269621e3ae3c405c2a339f23df48365190c57407
josepro752 <joseignacioboggio@gmail.com>
2023-09-18 16:14:26 -0300

    subo los metodos puts

--- Controllers/CadeteriaController.cs
+++ Controllers/CadeteriaController.cs
@@ -48,4 +48,34 @@ public class CadeteriaController : ControllerBase
         return StatusCode(500,"No se pudo tomar el pedido");
     }
 
+    [HttpPut ("Asignar_Pedido")]
+    public ActionResult <string> AsignarPedido(int idCadete, int numPedido) {
+        var pedido = cadeteria.Pedidos.FirstOrDefault(p => p.Numero == numPedido);
+        var cadete = cadeteria.Cadetes.FirstOrDefault(p => p.Id == idCadete);
+        if (pedido != null) {
+            if (cadete != null) {
+                pedido.IdCadete = idCadete;
+                return (Ok(pedido));
+            }
+            return StatusCode(500,"No se pudo encontrar el cadete");
+        }
+        return StatusCode(500,"No se pudo encontrar el pedido");
+    }
+
+    [HttpPut ("Cambiar_Estado_Pedido")]
+    public ActionResult <string> CambiarEstadoPedido(int numPedido, int estado) {
+        var pedido = cadeteria.Pedidos.FirstOrDefault(p => p.Numero == numPedido);
+        if (pedido != null) {
+            if (estado > 0 && estado < 4) {
+                pedido.Estado = (Estado)Enum.Parse(typeof(Estado),estado.ToString());
+                return (Ok(pedido));
+            }
+            return StatusCode(500,"El estado no es valido");
+        }
+        return StatusCode(500,"No se pudo encontrar el pedido");
+    }
+    [HttpPut ("Cambiar_Cadete_Pedido")]
+    public ActionResult <string> CambiarCadetePedido(int idCadete, int numPedido) {
+        return AsignarPedido(idCadete,numPedido);
+    }
 }
